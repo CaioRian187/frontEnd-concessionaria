042233b377feb6a1505437acff5ca15ec70b2638
CaioRian187 <caiorian246810@gmail.com>
2026-07-15 18:39:57 -0300

    feature: estruturando a rota para a pagina de carros

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { CarroPage } from "./pages/CarroPage"
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/carros" element={<CarroPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
