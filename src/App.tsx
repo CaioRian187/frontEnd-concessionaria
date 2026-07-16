bf21fd5b7af165e72e5ab31f58170c8b9d2aea5f
CaioRian187 <caiorian246810@gmail.com>
2026-07-16 09:16:48 -0300

    feature: Adicionando a rota home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CarroPage } from "./pages/CarroPage"
+import { HomePage } from "./pages/HomePage"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/carros" element={<CarroPage />} />
       </Routes>
     </BrowserRouter>
