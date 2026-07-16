6f051d16e1463e8c53b8b7ca0ab149736846bf46
CaioRian187 <caiorian246810@gmail.com>
2026-07-16 12:03:32 -0300

    hotfix: Adicionando o atributo value no props do componente

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,12 +2,14 @@ interface InputProps {
     className: string,
     type: string,
     placeholder: string,
+    value: string | number;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ className, type, placeholder, onChange }: InputProps) => {
+export const Input = ({ className, type, placeholder, value, onChange }: InputProps) => {
     return (
         <input
+            value={value}
             className={className}
             type={type}
             placeholder={placeholder}
